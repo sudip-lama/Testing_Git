b4a89b21a5c90bacb60de0b56000e7f8922668b6
Sudip Lama <sudiplama20@gmail.com>
2016-03-03 16:08:19 -0500

    Added SecondTestClass.java

---
+++ Testing/src/com/test/SecondTestClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.test;
+
+/**
+ *
+ * @author SUDIP
+ */
+public class SecondTestClass {
+public static void main(String []arg)
+{
+    System.out.println("This the second file in testing");
+}
+}
